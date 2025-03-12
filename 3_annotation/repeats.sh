d532553a896b9a3f79f4f462923bb805e808cf29
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-03-12 15:38:00 -0400

    Update repeats.sh

--- 3_annotation/repeats.sh
+++ 3_annotation/repeats.sh
@@ -1,16 +1,17 @@
-# RepeatModeler (version = 2.0.6)
+#RepeatModeler (version = 2.0.6)
 singularity pull dfam-tetools-latest.sif docker://dfam/tetools:latest
 
-# activate conda env with singularity
+#activate conda env with singularity
 conda activate repeatmodeler_env
 singularity run ../dfam-tetools-latest.sif
 
 BuildDatabase -name nucella_genome hifi_2kb_decontaminated.fa 
 
-# start an instance 
+#start an instance 
 singularity instance start ../dfam-tetools-latest.sif run_rm
 
-# run modeler and masker
+#run modeler and masker
 nohup singularity exec instance://run_rm RepeatModeler -LTRStruct -database nucella_genome -threads 35 &
+#xsmall for soft-masking
 nohup singularity exec instance://run_rm RepeatMasker -pa 35 -lib ../nucella_genome-families.fa -xsmall -gff ../hifi_2kb_decontaminated.fa &
 
