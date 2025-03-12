3716b779d35347f2160823f0881bc17e2d8da78d
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-03-12 15:38:24 -0400

    Update hifiasm.sh

--- 2_assembly/hifiasm.sh
+++ 2_assembly/hifiasm.sh
@@ -1,19 +1,19 @@
-# remove adapters
+#remove adapters
 porechop -i input_reads.fastq -o output_reads.fq
 
-# filter reads for quality 
+#filter reads for quality 
 NanoFilt -q 5 output_reads.fq > all6_q5.fastq
 
-# and length
+#and length
 seqkit -m 2000 -o all6_2kb.fastq -j10 output_reads.fq 
 
-# assemble, for me this ran overnight and was done in the morning
+#assemble, for me this ran overnight and was done in the morning
 nohup hifiasm -t50 --ont -o ONT_2kb.asm output_reads.fq > hifiasm.log 2>&1 &
 
-# extract primary contigs from fasta
+#extract primary contigs from fasta
 awk '/^S/{print ">"$2;print $3}' ONT_2kb.asm.bp.p_ctg.gfa > hifiasm_2kb.p_ctg.fa 
 
-# run BUSCO 
+#run BUSCO 
 busco \
   -i /home/meghan/nucella_genome/hifiasm/hifiasm_2kb.p_ctg.fa \
   -o busco_hifi \
