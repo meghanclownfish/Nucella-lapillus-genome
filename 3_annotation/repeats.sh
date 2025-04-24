2e418b2deb5c5a95d695cc355f2dc2578d44fb8f
Meghan Ford <78237587+meghanclownfish@users.noreply.github.com>
2025-04-24 15:10:54 -0400

    Update repeats.sh

--- 3_annotation/repeats.sh
+++ 3_annotation/repeats.sh
@@ -5,13 +5,14 @@ singularity pull dfam-tetools-latest.sif docker://dfam/tetools:latest
 conda activate repeatmodeler_env
 singularity run ../dfam-tetools-latest.sif
 
-BuildDatabase -name nucella_genome hifi_2kb_decontaminated.fa 
+BuildDatabase -name nucella_genome_april nlap_genome_no_mito_no_bac.filtered.fasta 
 
 #start an instance 
 singularity instance start ../dfam-tetools-latest.sif run_rm
 
-#run modeler and masker
-nohup singularity exec instance://run_rm RepeatModeler -LTRStruct -database nucella_genome -threads 35 &
-#xsmall for soft-masking
-nohup singularity exec instance://run_rm RepeatMasker -pa 35 -lib ../nucella_genome-families.fa -xsmall -gff ../hifi_2kb_decontaminated.fa &
+#run repeat modeler, this took 94:54:20  (hh:mm:ss) 
+nohup singularity exec instance://run_rm RepeatModeler -LTRStruct -database nucella_genome_april -threads 45 &
 
+#run repeat masker, this took 12 hours 
+nohup singularity exec instance://run_rm RepeatMasker -pa 35 -lib nucella_genome_april-families.fa -xsmall \
+-gff nlap_genome_no_mito_no_bac.filtered.fasta &
